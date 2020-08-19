ef34b4f0c939d4079d5e8433640a50586d326e98
Sanguan Anantathanasarn <asanguan@outlook.com>
2020-08-19 14:10:36 +0700

    200819

--- ViewLDD/frmMain.Designer.vb
+++ ViewLDD/frmMain.Designer.vb
@@ -1,6 +1,7 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
-    Inherits System.Windows.Forms.Form
+Partial Class frmData
+    'Inherits System.Windows.Forms.Form
+    Inherits MetroFramework.Forms.MetroForm
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
