57da62705ee06b30950d5001e539d8bb3943a4a1
Sanguan Anantathanasarn <asanguan@outlook.com>
2020-08-21 21:01:59 +0700

    200821

---
+++ ViewLDD/frmMain.Designer.vb
@@ -0,0 +1,44 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    'Inherits System.Windows.Forms.Form
+    Inherits MetroFramework.Forms.MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.SuspendLayout()
+        '
+        'frmMain
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackImage = Global.ViewLDD.My.Resources.Resources.LDDVIEWico
+        Me.BackImagePadding = New System.Windows.Forms.Padding(3)
+        Me.BackMaxSize = 24
+        Me.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle
+        Me.Name = "frmMain"
+        Me.ShadowType = MetroFramework.Forms.MetroFormShadowType.None
+        Me.Style = MetroFramework.MetroColorStyle.Pink
+        Me.TransparencyKey = System.Drawing.Color.Empty
+        Me.ResumeLayout(False)
+
+    End Sub
+End Class
